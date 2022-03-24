50fd022221136f3e2dfd45bc9c9c9742e00b2ed5
seirra blake <sophietheopossum@yandex.ru>
2022-03-24 21:55:34 +0000

    use java style declaration

--- src/ExampleApplet.java
+++ src/ExampleApplet.java
@@ -2,7 +2,7 @@ import processing.core.PApplet;
 
 public class ExampleApplet extends PApplet {
 
-    public static void main(String args[]) {
+    public static void main(String[] args) {
         PApplet.main("ExampleApplet");
     }
 
